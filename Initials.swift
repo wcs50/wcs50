dd02a9419c7f9c29da0e01b0346b6e31693fcd7e
Arunod Wickramasinghe <124810616+wcs50@users.noreply.github.com>
2023-09-19 10:57:43 +0530

    Update Initials.swift

--- Initials.swift
+++ Initials.swift
@@ -1,1 +1,8 @@
-
+//Arunod Wickramasinghe
+print("  A    W   W")
+print(" A A   W   W")
+print("A   A  W   W")
+print("AAAAA  W W W")
+print("A   A  WW WW")
+print("A   A  WW WW")
+print("A   A  W   W")
